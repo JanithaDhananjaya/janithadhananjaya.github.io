3bd85f6c506fbc33830333ea208ed8ce73023b2f
janithasilva <ajdsilva@zuelligpharma.com>
2026-03-21 14:10:50 +0530

    Remove basePath mapping as repo is now a User Site

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/janithasilva.github.io',
-  assetPrefix: '/janithasilva.github.io',
   images: {
     unoptimized: true,
   },
